23bf7d438f834a36821c22b8140725c5275fa28f
KNightFuryX <60267521+KNightFuryX@users.noreply.github.com>
2022-05-09 13:28:09 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchBar from "./SearchBar";
+import AddItem from "./AddItem";
+import { useState, useEffect } from "react";
+import ItemsDisplay from "./ItemsDisplay";
 
 function App() {
+  const [filters, setFilters] = useState({});
+  const [data, setData] = useState({ items: [] });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/items")
+      .then((response) => response.json())
+      .then((data) => setData({ items: data }));
+  }, []);
+
+  const updateFilters = (param) => {
+    setFilters(param);
+  };
+  const addItem = (item) => {
+    let items = data["items"];
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    };
+    fetch("http://localhost:3000/items", requestOptions)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        items.push(data);
+        setData({ items: items });
+        console.log(data);
+      });
+  };
+  const deleteItem = (item) => {
+    let items = data["items"];
+
+    const requestOptions = {
+      method: "DELETE",
+    };
+    fetch(`http://localhost:3000/items/${item.id}`, requestOptions).then(
+      (response) => {
+        if (response.ok) {
+          const index = items.indexOf(item);
+          items.splice(index, 1);
+          setData({ items: items });
+        }
+      }
+    );
+  };
+
+  const filterData = (data) => {
+    const filteredData = [];
+    if (!filters.name) {
+      return data;
+    }
+    for (const item of data) {
+      if (filters.name !== "" && item.name !== filters.name) continue;
+      if (filters.price !== "" && item.price > filters.price) continue;
+      if (filters.type !== "" && item.type !== filters.type) continue;
+      if (filters.brand !== "" && item.brand !== filters.brand) continue;
+      filteredData.push(item);
+    }
+    return filteredData;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row mt-3">
+        <SearchBar updateSearchParams={updateFilters} />
+      </div>
+      <div className="row mt-4">
+        <AddItem addItem={addItem} />
+      </div>
+      <div className="row mt-3">
+        <ItemsDisplay
+          items={filterData(data["items"])}
+          deleteItem={deleteItem}
+        />
+      </div>
     </div>
   );
 }
